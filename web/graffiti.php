430fff2fed1e14f772dc4449b90fb8a092f4fab5
Daniel Sikar <dsikar@gmail.com>
2017-02-25 13:29:27 +0000

    Moved sqrt function off NodeMCU to cloud.

--- web/graffiti.php
+++ web/graffiti.php
@@ -5,24 +5,48 @@
         Cloud based graffiti artist
 
         Driver software requests action with URI args:
-	1. x - x coordinate
-	2. y - y coordinate
-	3. p - spray can pressed status 0 - not pressed, 1 - pressed
-	4. action - browser flag, if present request comes from browser, if not present request comes from graffiti artist
+        1. x - x coordinate
+        2. y - y coordinate
+        3. p - spray can pressed status 0 - not pressed, 1 - pressed
+        4. action - browser flag, if present request comes from browser, if not present request comes from graffiti artist
+        NB x and y now mean length of hypotenuse for x and y steppers.
 */
+
+// configuration variables
+// margins (mm)
+$iMx = 150; 
+$iMy = 150;
+// canvas size (mm) as per canvas.php
+$iCx = 1100;
+$iCy = 700; // not used
+
 $action = (isset($_GET['action']) ? $_GET['action'] : null);
 if($action == "") {
-	$cmd = "tail -1 /tmp/graffiti.txt";
-	$cmdout = shell_exec($cmd);
-	$vars = explode("|", $cmdout);	
-	echo "x = $vars[0], y = $vars[1], p = $vars[2]\n";			
-	echo $cmdout;
+        $cmd = "tail -1 /tmp/graffiti.txt";
+        $cmdout = shell_exec($cmd);
+        $vars = explode("|", $cmdout);
+	$iHypX = getHypX($vars[0], $vars[1]);
+	$iHypY = getHypY($vars[0], $vars[1]);
+	echo "$iHypX|$iHypY|$vars[2]";
 } else {
-	$x = (isset($_GET['x']) ? $_GET['x'] : null);
-	$y = (isset($_GET['y']) ? $_GET['y'] : null);
-	$p = (isset($_GET['p']) ? $_GET['p'] : null);
-	$cmd = "echo \"$x|$y|$p\" >> /tmp/graffiti.txt";
-	echo $cmd;
-	shell_exec($cmd);
+        $x = (isset($_GET['x']) ? $_GET['x'] : null);
+        $y = (isset($_GET['y']) ? $_GET['y'] : null);
+        $p = (isset($_GET['p']) ? $_GET['p'] : null);
+        $cmd = "echo \"$x|$y|$p\" >> /tmp/graffiti.txt";
+        echo $cmd;
+        shell_exec($cmd);
 }
+
+function getHypX($x, $y) {
+        $b = $GLOBALS['iMx'] + $x;
+	$a = $GLOBALS['iMy'] + $y;
+	return intval(sqrt(pow($a, 2) + pow($b, 2)));
+}
+
+function getHypY($x, $y) {
+        $b = $GLOBALS['iMx'] + $GLOBALS['iCx'] - $x;
+        $a = $GLOBALS['iMy'] + $y;
+        return intval(sqrt(pow($a, 2) + pow($b, 2)));
+}
+
 ?>
