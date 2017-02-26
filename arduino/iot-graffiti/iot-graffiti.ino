11effc9114b199100c11d16e59bfd545b137e358
Daniel Sikar <dsikar@gmail.com>
2017-02-26 20:11:12 +0000

    Final tweaks before d-day.

--- arduino/iot-graffiti/iot-graffiti.ino
+++ arduino/iot-graffiti/iot-graffiti.ino
@@ -21,7 +21,7 @@
 ESP8266WiFiMulti WiFiMulti;
 
 // Graffiti-able area constants in mm
-#define iXLength 1100
+#define iXLength 1310
 #define iYLength 800
 // margins
 #define iMarginX 150 
@@ -35,20 +35,17 @@ String strDelim = "|";
 
 // Serial debug. Set to 1 to debug.
 #define SERIAL_DEBUG 0
-#define SERIAL_DEBUG_2 1
+#define SERIAL_DEBUG_2 0
 // Servo objects
 Servo sprayServo;
 // Servo pins
-int sprayPin = 5; // D1
+int sprayPin = 2; // D4
 
 // Stepper pins
-int iDirX = 4; // D2
-int iStepX = 0; // D3
-int iDirY = 2; // D4
-int iStepY = 14; // D5
-
-// LED pin - web access feedback
-int iLEDPin = 16; // NodeMCU LED pin
+int iDirX = 16; // D0
+int iStepX = 5; // D1
+int iDirY = 4; // D2
+int iStepY = 0; // D3
 
 // Stepper step delay (less delay = faster)
 int iStepDelay = 2;
@@ -62,12 +59,15 @@ int iNextPosX = 0; // or initial position e.g. 50
 int iNextPosY = 0; // or initial position e.g. 50
 
 // Current Position - TODO SET in setup();
-int iCurrPosX = 64; //cm
-int iCurrPosY = 64; //cm
+int iCurrPosX = 1100; //cm
+int iCurrPosY = 1100; //cm
 
 // multiplier ~ turn mm into steps
 // int iCoordMult = 47; // (46.94836 steps move 1cm)
-float fCoordMult = 4.694836; // steps to move 1mm
+float fCoordMult = 4.7; // steps to move 1mm
+
+// Http delay - how often to pool web page
+int iHttpDelay = 50;
 
 void setup() {
 
@@ -92,7 +92,7 @@ void setup() {
     for(uint8_t t = 4; t > 0; t--) {
         USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
         USE_SERIAL.flush();
-        delay(1000);
+        delay(3000);
     }
 
     // Commented out for debugging - COMMENT IN ONCE WORKING
@@ -141,7 +141,6 @@ void loop() {
         if(httpCode > 0) {
             // HTTP header has been send and Server response header has been handled
             //USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-
             // file found at server
             if(httpCode == HTTP_CODE_OK) {
                 String payload = http.getString();
@@ -151,61 +150,10 @@ void loop() {
         } else {
             USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
         }
-
         http.end();
-    }
-    setHigh(iLEDPin);
-    delay(1000);
-    setLow(iLEDPin);
+    } 
+    delay(iHttpDelay);
 }
-
-/*
-void parse(String strCmd) {
- if(strCmd == "LT") {
-   USE_SERIAL.println("Left Turn");
-   int iNextPos = pos - 10;
-  USE_SERIAL.println("pos before call =");
-  USE_SERIAL.println(pos);
-   // for(iCurrPos; iCurrPos <= iNextPos; iCurrPos -= 1) // goes from 0 degrees to 180 degrees 
-  for(pos; pos>=iNextPos; pos-=1)
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(40);                       // waits 15ms for the servo to reach the position 
-  } 
-  USE_SERIAL.println("pos after call =");
-  USE_SERIAL.println(pos);
- } 
- if(strCmd == "RT") {
-   //USE_SERIAL.println("Right Turn");
-   int iNextPos = pos + 10;
-   // for(iCurrPos; iCurrPos <= iNextPos; iCurrPos -= 1) // goes from 0 degrees to 180 degrees 
-  for(pos; pos<=iNextPos; pos+=1)
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(40);                       // waits 15ms for the servo to reach the position 
-  }  
-  //USE_SERIAL.println("pos =");
-  //USE_SERIAL.println(pos);  
- }
- if(strCmd == "FW") {
-   //USE_SERIAL.println("Forward");
-   myservo2.writeMicroseconds(iGo);
-   delay(15); 
-   
- }
- if(strCmd == "RV") {
-   USE_SERIAL.println("Reverse");
-   myservo2.writeMicroseconds(iReverse);
-   delay(15); 
-   
- } 
- if(strCmd == "ST") {
-   USE_SERIAL.println("Stop");
-   myservo2.writeMicroseconds(iStop);
-   delay(15); 
- } 
-}
-*/
 
 void sprayOn(Servo myServo) {
   /*
@@ -216,7 +164,7 @@ void sprayOn(Servo myServo) {
     delay(15);                       // waits 15ms for the servo to reach the position 
   } 
   */
-  int pos = 90;
+  int pos = 65;
   myServo.write(pos);
   delay(15);
 }
@@ -319,31 +267,6 @@ void getSetXY(String strGraffiti) {
 }
 
 /*
-  setval - legacy function from cinema4d rig
-*/
-
-void setval(String txtMsg)
-{
-    String strStepper = txtMsg.substring(0,1);
-    String pos = txtMsg.substring(1);
-    if (strStepper == "H") {
-      //myservoH.write(data);
-      iH = pos.toInt();    
-      cnt ++;
-    }
-    if (strStepper == "V"){
-    //myservoV.write(data);
-    iV = pos.toInt();
-    cnt ++;
-    }
-    if (cnt % 2 == 0)
-    {
-    setPos(iH, iV, 0);
-    delay(15);
-    }
-}
-
-/*
   setPos - set x and y timing belt lengths
 */
 
@@ -354,28 +277,17 @@ void setPos(int iX, int iY, int iSpray) {
   // a wall - spraying against wall
   // Do not mirror is canvas is mounted on rig -
   // spraying against rig
-  
+
   // if we are spraying
   if(iSpray) {
-    if(SERIAL_DEBUG) {
-      USE_SERIAL.print("x = ");
-      USE_SERIAL.println(iX);
-      USE_SERIAL.print("y = ");
-      USE_SERIAL.println(iY); 
-    }
-    
-    int iHypX = getHypX(iX, iY);
-    int iHypY  = getHypY(iX, iY);
    
-     if(SERIAL_DEBUG_2) {
-      USE_SERIAL.print("iHypX = ");
-      USE_SERIAL.println(iHypX);
-      USE_SERIAL.print("iHypY = ");
-      USE_SERIAL.println(iHypY); 
-    }
-    // position spray can
-    windX(iHypX);
-    windY(iHypY);
+    // move sqrt to cloud, perfoming poorly on embedded CPU
+    // int iHypX = getHypX(iX, iY);
+    // int iHypY  = getHypY(iX, iY);
+ 
+    // position spray can only deal with x for now
+    windX(iX);
+    windY(iY);
     
     // press spray
     sprayOn(sprayServo);
@@ -387,36 +299,15 @@ void setPos(int iX, int iY, int iSpray) {
   }
 }
 
-int getHypX(int iX, int iY) {
-        // my ($i, $r) = @_;
-        int b = iMarginX + iX;
-        int a = iMarginY + (iYLength - iY);
-        // add 0.5 to round to nearest integer
-        int iHypX = (sqrt(a * a + b * b) + 0.5);
-        return iHypX;
-}
-
-int getHypY(int iX, int iY) {
-        // my ($i, $r) = @_;
-        int b = iMarginX + (iXLength - iX);
-        int a = iMarginY + (iYLength - iY);
-        // add 0.5 to round to nearest integer
-        int iHypY = (sqrt(a * a + b * b) + 0.5);
-        return iHypY;
-}
-
-void moveX(int iX) {
-  // Subtract the required position from the current position
-  // e.g. current = 25, required = 10 ~ move = required - current = -15
-  windX(iX - iCurrPosX);
-  iCurrPosX = abs(iX); // e.g. current position after move ~ 
-}
-
 void windX(int iX) {
+    if(SERIAL_DEBUG) {
+      USE_SERIAL.print("windX(int iX)");
+    }
+      
   int iMove = iX - iCurrPosX;
    if(SERIAL_DEBUG) {
-    USE_SERIAL.print("windX(int iX) ~ iX = ");
-    USE_SERIAL.println(iX);
+    USE_SERIAL.print("iMove = ");
+    USE_SERIAL.println(iMove);
     USE_SERIAL.print("abs(iX - iCurrPosX) = ");
     USE_SERIAL.println(abs(iX - iCurrPosX)); 
   }
@@ -425,12 +316,14 @@ void windX(int iX) {
     if(SERIAL_DEBUG) {
       USE_SERIAL.print("setHigh(iDirX)"); 
     }     
-    setLow(iDirX);
+    // setLow(iDirX);
+    setHigh(iDirX);
   } else {
     if(SERIAL_DEBUG) {
       USE_SERIAL.print("setLow(iDirX)"); 
     }     
-    setHigh(iDirX);
+    // setHigh(iDirX);
+    setLow(iDirX);
   }
   float fTotalSteps = abs(iMove) * fCoordMult;
   int iTotalSteps = fTotalSteps;
@@ -452,9 +345,11 @@ void windX(int iX) {
 void windY(int iY) {
   int iMove = iY - iCurrPosY;  
   if(iMove > 0) {
-    setHigh(iDirY);
-  } else {
+    // setHigh(iDirY);
     setLow(iDirY);
+  } else {
+    // setLow(iDirY);
+    setHigh(iDirY);
   }
   float fTotalSteps = abs(iMove) *  fCoordMult;
   int iTotalSteps = fTotalSteps;
